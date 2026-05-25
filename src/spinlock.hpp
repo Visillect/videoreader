bbe665b75e3300139fba414f34b6cca3ac9e281c
Arseniy Terekhin <senyai@gmail.com>
2026-05-25 18:18:16 +0300

    feat: ensure `SpinLock` is not moveable

--- src/spinlock.hpp
+++ src/spinlock.hpp
@@ -4,6 +4,15 @@ class SpinLock {
   std::atomic_flag lck = ATOMIC_FLAG_INIT;
 
 public:
+  SpinLock() = default;
+  ~SpinLock() = default;
+
+  // non-copyable, non-movable
+  SpinLock(SpinLock const&) = delete;
+  SpinLock& operator=(SpinLock const&) = delete;
+  SpinLock(SpinLock&&) = delete;
+  SpinLock& operator=(SpinLock&&) = delete;
+
   void lock() {
     while (lck.test_and_set(std::memory_order_acquire))
       ;
